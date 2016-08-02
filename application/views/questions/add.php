f7bd2867b2da972151caa6567515b311700cb150
elex <elextraza@gmail.com>
2016-08-02 09:54:31 +0300

    view обавления вопроса к лекции

---
+++ application/views/questions/add.php
@@ -0,0 +1,58 @@
+<div class="center-container">
+    <h1 class="main-h1">Редактирование вопроса</h1>
+
+    <div class="w-form">
+        <form class="w-clearfix" id="email-form-5" name="email-form-5">
+            <label for="question-text">Текст вопроса:</label>
+            <textarea class="question-text w-input" id="question-text" maxlength="5000" name="question-text"></textarea>
+
+            <div class="w-row">
+                <div class="w-col w-col-3">
+                    <div class="answer-item edit">
+                        <label for="field-4">Ответ 1:</label>
+                        <input class="w-input" id="field-4" maxlength="256" name="field-4" placeholder="Example Text"  type="text">
+
+                        <div class="w-checkbox">
+                            <input class="w-checkbox-input" id="checkbox-8" name="checkbox-8" type="checkbox">
+                            <label class="w-form-label" for="checkbox-8">Правельный</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-col w-col-3">
+                    <div class="answer-item edit">
+                        <label for="field-5">Ответ 2:</label>
+                        <input class="w-input" id="field-5" maxlength="256" name="field-5" placeholder="Example Text" type="text">
+
+                        <div class="w-checkbox">
+                            <input class="w-checkbox-input" data-name="Checkbox 9" id="checkbox-9" name="checkbox-9" type="checkbox">
+                            <label class="w-form-label" for="checkbox-9">Правельный</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-col w-col-3">
+                    <div class="answer-item edit">
+                        <label for="field-6">Ответ 3:</label>
+                        <input class="w-input" id="field-6" maxlength="256" name="field-6" placeholder="Example Text" type="text">
+
+                        <div class="w-checkbox">
+                            <input class="w-checkbox-input" id="checkbox-10" name="checkbox-10" type="checkbox">
+                            <label class="w-form-label" for="checkbox-10">Правельный</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-col w-col-3">
+                    <div class="answer-item edit">
+                        <label for="field-7">Ответ 4:</label>
+                        <input class="w-input" id="field-7" maxlength="256" name="field-7" placeholder="Example Text" type="text">
+
+                        <div class="w-checkbox"><input class="w-checkbox-input" id="checkbox-11" name="checkbox-11" type="checkbox">
+                            <label class="w-form-label" for="checkbox-11">Правельный</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <input class="user-button w-button" type="submit" value="Сохранить">
+        </form>
+
+    </div>
+</div>
